de5aa079f3f90fd5fff5e0a956843e28b6556a3a
Antoine LOIZEAU <antoine.loizeau@hotmail.fr>
2022-06-22 15:48:00 +0000

    Clean the code

--- BlazorWebApp/Data/FunctionAPI.cs
+++ BlazorWebApp/Data/FunctionAPI.cs
@@ -7,15 +7,13 @@ namespace BlazorWebApp.Data
         public async Task<string> GetSampleAsync()
         {
             // Acquire the access token.
-            string[] scopes = new string[] { "user.read" };
-
             var azureServiceTokenProvider = new AzureServiceTokenProvider();
             string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("api://08e708ba-8da4-411d-8c7e-8aff9ddb6c3e");
 
             // Use the access token to call a protected web API.
             var client = new HttpClient();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            var response = await client.GetAsync("https://imi-securedfunction.azurewebsites.net/api/Sample");
+            var response = await client.GetAsync("https://imi-securedfunction.azurewebsites.net/api/Sample?code=ZGfh3gf66aTWcwNff0fMwMmR_M41kZl5gbeKYIufP8CRAzFuUAR-qA==");
             return await response.Content.ReadAsStringAsync() + "| accessToken: " + accessToken;
         }
     }
